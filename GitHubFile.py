93fa5f9e29e8c078b65137a22d80794070ccc0c5
T_Gayden <gayden7@gmail.com>
2020-02-19 19:15:06 -0500

    Initial commit

---
+++ GitHubFile.py
@@ -0,0 +1,17 @@
+import csv
+
+f = open("7-100_Sales-Records.csv", 'rt')
+
+reader = csv.reader(f)
+counter=0
+i = 0
+total = 0
+for row in reader:
+    if row[0] == "Europe":
+        counter+=1
+    if i !=0:
+        total+=int(row[8])
+    i+=1
+
+print("No of Europe is %d" %counter)
+print("total number of items sold is %d" %total)
